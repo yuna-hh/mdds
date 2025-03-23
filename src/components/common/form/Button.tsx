f3c0d400b64da8ebeafacad2ebc069306126d597
yuna-hh <ghddbsk12@naver.com>
2025-03-23 17:31:29 +0900

    feat: Button 컴포넌트 완료

--- src/components/common/form/Button.tsx
+++ src/components/common/form/Button.tsx
@@ -1,10 +1,12 @@
+import throttle from "lodash.throttle";
 import Link from "next/link";
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, MouseEvent, useCallback } from "react";
 
 type ButtonProps = {
   content: string;
   variant?: "default" | "option";
   href?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 } & ComponentProps<"button">;
 
 const bgColor = {
@@ -16,10 +18,21 @@ const Button = ({
   content,
   variant = "default",
   href,
+  onClick,
   ...props
 }: ButtonProps) => {
+  const throttledClick = useCallback(
+    throttle((event: MouseEvent<HTMLButtonElement>) => {
+      if (onClick) {
+        onClick(event);
+      }
+    }, 800),
+    [onClick]
+  );
+
   // 추후에 스타일 보면서 변경하기
   const baseButtonStyle = `block w-[225px] py-[20px] text-2xl font-bold text-center text-white rounded-lg ${bgColor[variant]}`;
+
   if (href) {
     return (
       <Link href={href} className={baseButtonStyle}>
@@ -28,7 +41,7 @@ const Button = ({
     );
   }
   return (
-    <button {...props} className={baseButtonStyle}>
+    <button className={baseButtonStyle} onClick={throttledClick} {...props}>
       {content}
     </button>
   );
