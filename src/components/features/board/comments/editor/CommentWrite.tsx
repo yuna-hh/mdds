83ec150307ff200d34c1394adc382599350428bc
yuna-hh <ghddbsk12@naver.com>
2025-05-17 00:44:41 +0900

    feat: 공백 유효성검사 추가

--- src/components/features/board/comments/editor/CommentWrite.tsx
+++ src/components/features/board/comments/editor/CommentWrite.tsx
@@ -15,7 +15,7 @@ const CommentWrite = () => {
   return (
     <form className="flex flex-row border border-main-1 rounded-lg">
       <div className="flex flex-col grow-6 py-3 px-4">
-        <span className="font-semibold">
+        <span className="font-semibold mb-[10px]">
           {user?.user_metadata.display_name}
         </span>
         <textarea
@@ -23,7 +23,7 @@ const CommentWrite = () => {
           placeholder="댓글을 작성해주세요"
           className="grow-1 outline-none resize-none"
           {...register("content")}
-        ></textarea>
+        />
       </div>
       <button className="grow-1 py-[55px] border-l border-main-1 font-bold">
         등록
