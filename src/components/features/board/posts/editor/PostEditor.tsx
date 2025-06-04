65625025d117f4b96c3a3fb2f83cedbceac44493
yuna-hh <ghddbsk12@naver.com>
2025-06-04 19:06:08 +0900

    feat: yarn-build 3차 수정

--- src/components/features/board/posts/editor/PostEditor.tsx
+++ src/components/features/board/posts/editor/PostEditor.tsx
@@ -19,12 +19,13 @@ const PostEditor = ({ postId, isEdit }: PostWriteType) => {
   const { data: prevPostData } = useGetPost(postId);
   const router = useRouter();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (prevPostData && user?.id !== prevPostData.author) {
       Notify.failure("비정상적인 접근입니다");
       router.push("/");
     }
-  }, [prevPostData, user]); // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [prevPostData, user]);
 
   if (!categoryData) return <Loading />;
 
