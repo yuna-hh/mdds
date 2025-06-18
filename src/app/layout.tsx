2803f30daad8282a69b05c8d9a7d74e46d250026
yuna-hh <ghddbsk12@naver.com>
2025-06-19 03:05:17 +0900

    feat: manifest 설정 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,20 @@ import NotiflixProvider from "./(providers)/_providers/NotiflixProvider";
 export const metadata: Metadata = {
   title: "물댄동산",
   description: "물댄동산 지출증빙 내역 사이트",
+  metadataBase: new URL("https://md-ds.vercel.app/"),
   icons: {
-    icon: "/favicon.ico",
+    icon: "/logo/favicon.ico",
+    apple: "/logo/logo-ios.png",
   },
+  openGraph: {
+    images: [
+      {
+        url: "/logo/mdds-og.png",
+        alt: "물댄동산 지출증빙 내역 사이트",
+      },
+    ],
+  },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
